f8178d48bc505fc582c1ced354512c5600b7cb1a
Valentin Mega <MegaValentin@users.noreply.github.com>
2022-06-04 23:38:56 -0300

    ..

--- src/app.py
+++ src/app.py
@@ -1,3 +1,5 @@
+from crypt import methods
+from urllib import request
 from flask import Flask, render_template
 
 app=Flask(__name__)
@@ -6,6 +8,9 @@ app=Flask(__name__)
 def index():
     return render_template('index.html')
 
-
+@app.route('/user' , methods=['POST'])
+def usuario():
+    nombre = request.form['nombre']
+    return render_template('usuario.html', nombre=nombre)
 
     
