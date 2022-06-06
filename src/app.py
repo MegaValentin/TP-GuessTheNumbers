aaa6c9c194111251e45b9180c25fa14bbfc08c2a
Valentin Mega <MegaValentin@users.noreply.github.com>
2022-06-05 21:41:24 -0300

    se agrego el email

--- src/app.py
+++ src/app.py
@@ -3,14 +3,16 @@ from urllib import request
 from flask import Flask, render_template
 
 app=Flask(__name__)
-#primer ruta
+#ruta a la plantilla principal
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/user' , methods=['POST'])
 def usuario():
+    #datos del usuario para comenzar el juego
     nombre = request.form['nombre']
-    return render_template('usuario.html', nombre=nombre)
+    email = request.form['email']
+    return render_template('usuario.html', nombre=nombre, email=email)
 
     
