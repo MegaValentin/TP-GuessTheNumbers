30fae86e989192cd2492bc0285f4e1a32a7c5262
Valentin Mega <MegaValentin@users.noreply.github.com>
2022-06-14 12:39:08 -0300

    ...
    se agrego css

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from domain.User import User 
 from juego import juego #archivo - clase
 
