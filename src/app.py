d709abae41823aff9050ddfb9dc358d1b86ecbc7
Valentin Mega <MegaValentin@users.noreply.github.com>
2022-06-14 10:01:04 -0300

    ingresar los 5 numeros

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
-from crypt import methods
 from flask import Flask, render_template, request
 from domain.User import User 
+from juego import juego #archivo - clase
 
 
 app=Flask(__name__)
@@ -13,14 +13,26 @@ def index():
 def usuario():
     #datos del usuario para comenzar el juego
     usuario = request.form['usuario']
-    email = request.form['email']
+    
+    return render_template('user.html', usuario=usuario)
 
+@app.route('/game', methods=['GET','POST'])
+def game(): 
+    if request.method == "GET":
 
-    return render_template('user.html', usuario=usuario, email=email)
+        return render_template('user.html')
 
-@app.route('/game', methods=['GET']['POST'])
+    elif request.method == "POST":
 
+        numero1= request.form['1']
+        numero2= request.form['2']
+        numero3= request.form['3']
+        numero4= request.form['4']
+        numero5= request.form['5']
 
+        numerosDelUsuario = [int(numero1), int(numero2), int(numero3), int(numero4), int(numero5)]
 
-if __name__=='__main__':
-    app.run(port=3000, debug=True)
+        tiempo, numerosIngresados = juego.compararAmbasListas(5, numerosDelUsuario)
+
+        return render_template('user.html', tiempo = tiempo, numerosIngresados = numerosIngresados)
+        
--- src/juego.py
+++ src/juego.py
@@ -1,127 +1,121 @@
 import random
 from datetime import datetime
 
-def horaDeLaPartida():
-    
-    hora = datetime.now() 
-    horaactual=datetime.strftime(hora,'%d/%m/%Y %H:%M:%S')
-    print(horaactual)
-
-def nombreUsuario():
-    nombre = ""
-    nombre = input("ingrese un nombre de usuario: ")
-    print(f'Hola {nombre}. ¡Bienvenido a Guess The Number!')
-
-def randomDe5Numeros(valorMaximo):
-    
-    """Genera una lista de 5 elementos aleatorios.
-    Estos 5 elementos seran los que el usuario debera adivinar"""
-
-    numerosAAdivinar = []
-    rango=0
-    cantidadDeElemen = 5
-    
-    while rango < cantidadDeElemen:
-
-        numerosRandom = (random.randint(0, valorMaximo))
+class juego:
+    def horaDeLaPartida():
         
-        if numerosRandom not in numerosAAdivinar:
-            numerosAAdivinar.append(numerosRandom)
-            rango+=1
-    
-    return numerosAAdivinar
+        hora = datetime.now() 
+        horaactual=datetime.strftime(hora,'%d/%m/%Y %H:%M:%S')
+        print(horaactual)
 
-def numerosIngresadosPorElUsuario():
-    
-    """Pide al usuario que ingrese los valores que queria en un rango de 0 
-    a 10 desde la posicion 1 a la posicion 5"""
-
-    numerosIngresados = []
-    rango = 0
-    cantidadDeElemen = 5
-    
-    while rango < cantidadDeElemen:
-
-        numerosDelUsuario = int(input("ingrese un numero entre 0 al 10 : "))
-
+    def randomDe5Numeros(valorMaximo):
         
-        if numerosDelUsuario < 11:
-            if numerosDelUsuario not in numerosIngresados:
-                numerosIngresados.append(numerosDelUsuario)
-                rango +=1
-            else:
-                print("No es valido ingresar numeros iguales")
+        """Genera una lista de 5 elementos aleatorios.
+        Estos 5 elementos seran los que el usuario debera adivinar"""
+
+        numerosAAdivinar = []
+        rango=0
+        cantidadDeElemen = 5
+        
+        while rango < cantidadDeElemen:
+
+            numerosRandom = (random.randint(0, valorMaximo))
             
-        else:
-            print("Debes colocar una cifra en el rango solicitado")
+            if numerosRandom not in numerosAAdivinar:
+                numerosAAdivinar.append(numerosRandom)
+                rango+=1
         
-    
-    return numerosIngresados
+        return numerosAAdivinar
 
-def compararAmbasListas(cantidadDeIntentos):
-
-    """Compara ambas listas(lista random generada y lista armada por el ususario) en una cantidad de intentos"""
-
-    verde = []
-    amarillo = []
-    numerosAAdivinar = []
-    #listaRandom = [1, 2, 3, 4, 5]
-    listaRandom = randomDe5Numeros(10)
-    jugadas = 0
-    intentos = cantidadDeIntentos 
-    nombre=nombreUsuario()
-
-    nombre
-    horaDeLaPartida()
-    """Ocultar los numeros que el usuario debe adivinar"""
-    for i in range(len(listaRandom)):
-        numerosAAdivinar.append('*')
-    print(numerosAAdivinar)
-
-    #verde = Posicion y numero correcto
-    #amarillo = Numero correcto pero no la posicion
-    #ningunColor = Posicion y numero incorrecto
-         
-    listaUsuario = numerosIngresadosPorElUsuario()
-    
-    while (True):
-        print("---------------------------------------------")
-        print(f'Jugada {jugadas + 1}. Te quedan {intentos - (jugadas + 1)} intentos')
+    def numerosIngresadosPorElUsuario():
         
-        for nu in range(len(listaUsuario)):
-            for nr in range(len(listaRandom)):
+        """Pide al usuario que ingrese los valores que queria en un rango de 0 
+        a 10 desde la posicion 1 a la posicion 5"""
 
-                if nu == nr :
+        numerosIngresados = []
+        rango = 0
+        cantidadDeElemen = 5
+        
+        while rango < cantidadDeElemen:
 
-                    if listaUsuario[nu] == listaRandom[nr]:
-                        verde.append(listaUsuario[nu])
+            numerosDelUsuario = int(input("ingrese un numero entre 0 al 10 : "))
 
+            
+            if numerosDelUsuario < 11:
+                if numerosDelUsuario not in numerosIngresados:
+                    numerosIngresados.append(numerosDelUsuario)
+                    rango +=1
                 else:
-                    if listaUsuario[nu] == listaRandom[nr]:
-                        amarillo.append(listaUsuario[nu])
-
-        if len(verde) == 5:
-            print("Has ganado")
-            return horaDeLaPartida()
-              
-        else:    
-            jugadas += 1 
+                    print("No es valido ingresar numeros iguales")
+                
+            else:
+                print("Debes colocar una cifra en el rango solicitado")
             
-            print(f'Numeros ingresados---> {listaUsuario}')       
-            print(f'Verde---> {verde}')
-            print(f'Amarillo---> {amarillo}')
-            print('Con los datos obtenidos ingrese nuevamente 5 numeros')
-            print("---------------------------------------------")
-
-        if jugadas >= intentos:
-            print("Has perdido :(")
-            print(f'Numeros ingresados---> {listaUsuario}')       
-            print(f'Los numeros a adivinar eran---> {listaRandom} ')
-            return horaDeLaPartida()
         
-        verde.clear()
-        amarillo.clear()
-        listaUsuario.clear()
-        listaUsuario = numerosIngresadosPorElUsuario()
+        return numerosIngresados
+    def compararAmbasListas(cantidadDeIntentos, numerosDelUsuario):
 
-compararAmbasListas(5)
+        """Compara ambas listas(lista random generada y lista armada por el ususario) en una cantidad de intentos"""
+
+        verde = []
+        amarillo = []
+        numerosAAdivinar = []
+        listaRandom = [1, 2, 3, 4, 5]
+        # listaRandom = juego.randomDe5Numeros(10)
+        jugadas = 0
+        intentos = cantidadDeIntentos 
+
+        juego.horaDeLaPartida()
+        """Ocultar los numeros que el usuario debe adivinar"""
+        for i in range(len(listaRandom)):
+            numerosAAdivinar.append('*')
+        print(numerosAAdivinar)
+
+        #verde = Posicion y numero correcto
+        #amarillo = Numero correcto pero no la posicion
+        #ningunColor = Posicion y numero incorrecto
+
+        
+        while (True):
+            print("---------------------------------------------")
+            print(f'Jugada {jugadas + 1}. Te quedan {intentos - (jugadas + 1)} intentos')
+            
+            for nu in range(len(numerosDelUsuario)):
+                for nr in range(len(listaRandom)):
+
+                    if nu == nr :
+
+                        if numerosDelUsuario[nu] == listaRandom[nr]:
+                            verde.append(numerosDelUsuario[nu])
+
+                    else:
+                        if numerosDelUsuario[nu] == listaRandom[nr]:
+                            amarillo.append(numerosDelUsuario[nu])
+
+            if len(verde) == 5:
+                print("Has ganado")
+                return juego.horaDeLaPartida(), numerosDelUsuario
+                
+           
+            elif jugadas >= intentos:
+                print("Has perdido :(")
+                print(f'Numeros ingresados---> {numerosDelUsuario}')       
+                print(f'Los numeros a adivinar eran---> {listaRandom} ')
+                return juego.horaDeLaPartida(), numerosDelUsuario
+
+            else:    
+                jugadas += 1 
+                
+                print(f'Numeros ingresados---> {numerosDelUsuario}')       
+                print(f'Verde---> {verde}')
+                print(f'Amarillo---> {amarillo}')
+                print('Con los datos obtenidos ingrese nuevamente 5 numeros')
+                print("---------------------------------------------")
+
+
+            verde.clear()
+            amarillo.clear()
+
+            return "12", numerosDelUsuario
+
+    
